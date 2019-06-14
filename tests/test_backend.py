dea19a2f702dbc7be1756999c9f39c93de5f7a9b
692307 <gaojiaying@buaa.edu.cn>
2019-06-14 16:18:00 +0800

    add gemm_rewriter and the corresponding tests

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -2494,5 +2494,83 @@ class BackendTests(Tf2OnnxBackendTestBase):
         _ = tf.identity(y, name=_TFOUTPUT)
         self._run_test_case([_OUTPUT], {_INPUT: x_val})
 
+    # test for gemm pattern0: alpha*A*B + beta*C
+    def test_gemm_pattern0(self):
+        iMaxSizeNumber = 10
+        iM = np.random.randint(iMaxSizeNumber)
+        iN = np.random.randint(iMaxSizeNumber)
+        iK = np.random.randint(iMaxSizeNumber)
+        x_val1 = np.random.rand(iM, iN).astype("float32")
+        x_val2 = np.random.rand(iN, iK).astype("float32")
+        x_val3 = np.random.rand(iM, iK).astype("float32")
+
+        a = tf.placeholder(tf.float32, x_val1.shape, name=_TFINPUT)
+        b = tf.placeholder(tf.float32, x_val2.shape, name=_TFINPUT1)
+        c = tf.placeholder(tf.float32, x_val3.shape, name=_TFINPUT2)
+        alpha = tf.constant(1.0, dtype=tf.float32, name='alpha')
+        beta = tf.constant(2.0, dtype=tf.float32, name='beta')
+        mul1 = tf.multiply(alpha, tf.matmul(a, b))
+        mul2 = tf.multiply(beta, c)
+        x_ = mul1 + mul2
+        _ = tf.identity(x_, name=_TFOUTPUT)
+        self._run_test_case([_OUTPUT], {_INPUT: x_val1, _INPUT1: x_val2, _INPUT2: x_val3},
+                            graph_validator=lambda g: check_op_count(g, "Gemm", 1))
+
+    # test for gemm pattern1: alpha*A*B + C
+    def test_gemm_pattern1(self):
+        iMaxSizeNumber = 10
+        iM = np.random.randint(iMaxSizeNumber)
+        iN = np.random.randint(iMaxSizeNumber)
+        iK = np.random.randint(iMaxSizeNumber)
+        x_val1 = np.random.rand(iM, iN).astype("float32")
+        x_val2 = np.random.rand(iN, iK).astype("float32")
+        x_val3 = np.random.rand(iM, iK).astype("float32")
+
+        a = tf.placeholder(tf.float32, x_val1.shape, name=_TFINPUT)
+        b = tf.placeholder(tf.float32, x_val2.shape, name=_TFINPUT1)
+        c = tf.placeholder(tf.float32, x_val3.shape, name=_TFINPUT2)
+        alpha = tf.constant(1.0, dtype=tf.float32, name='alpha')
+        x_ = tf.multiply(alpha, tf.matmul(a, b)) + c
+        _ = tf.identity(x_, name=_TFOUTPUT)
+        self._run_test_case([_OUTPUT], {_INPUT: x_val1, _INPUT1: x_val2, _INPUT2: x_val3},
+                        graph_validator=lambda g: check_op_count(g, "Gemm", 1))
+
+    # test for gemm pattern2: A*B + beta*C
+    def test_gemm_pattern2(self):
+        iMaxSizeNumber = 10
+        iM = np.random.randint(iMaxSizeNumber)
+        iN = np.random.randint(iMaxSizeNumber)
+        iK = np.random.randint(iMaxSizeNumber)
+        x_val1 = np.random.rand(iM, iN).astype("float32")
+        x_val2 = np.random.rand(iN, iK).astype("float32")
+        x_val3 = np.random.rand(iM, iK).astype("float32")
+
+        a = tf.placeholder(tf.float32, x_val1.shape, name=_TFINPUT)
+        b = tf.placeholder(tf.float32, x_val2.shape, name=_TFINPUT1)
+        c = tf.placeholder(tf.float32, x_val3.shape, name=_TFINPUT2)
+        beta = tf.constant(2.0, dtype=tf.float32, name='beta')
+        x_ = tf.matmul(a, b) + tf.multiply(beta, c)
+        _ = tf.identity(x_, name=_TFOUTPUT)
+        self._run_test_case([_OUTPUT], {_INPUT: x_val1, _INPUT1: x_val2, _INPUT2: x_val3},
+                            graph_validator=lambda g: check_op_count(g, "Gemm", 1))
+
+    # test for gemm pattern3: A*B + C
+    def test_gemm_pattern3(self):
+        iMaxSizeNumber = 10
+        iM = np.random.randint(iMaxSizeNumber)
+        iN = np.random.randint(iMaxSizeNumber)
+        iK = np.random.randint(iMaxSizeNumber)
+        x_val1 = np.random.rand(iM, iN).astype("float32")
+        x_val2 = np.random.rand(iN, iK).astype("float32")
+        x_val3 = np.random.rand(iM, iK).astype("float32")
+
+        a = tf.placeholder(tf.float32, x_val1.shape, name=_TFINPUT)
+        b = tf.placeholder(tf.float32, x_val2.shape, name=_TFINPUT1)
+        c = tf.placeholder(tf.float32, x_val3.shape, name=_TFINPUT2)
+        x_ = tf.matmul(a, b) + c
+        _ = tf.identity(x_, name=_TFOUTPUT)
+        self._run_test_case([_OUTPUT], {_INPUT: x_val1, _INPUT1: x_val2, _INPUT2: x_val3},
+                            graph_validator=lambda g: check_op_count(g, "Gemm", 1))
+
 if __name__ == '__main__':
     unittest_main()
---
+++ tf2onnx/rewriter/gemm_rewriter.py
@@ -0,0 +1,134 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+"""
+tf2onnx.rewrite - rewrite tensorflow subgraph to onnx gemm op
+"""
+
+from tf2onnx.graph_matcher import OpTypePattern, GraphMatcher
+
+# pylint: disable=missing-docstring
+
+def rewrite_gemm(g, ops):
+    if g.opset <= 6:
+        return ops
+
+    """
+    4 Candidate patterns are listed as follow, i.e. pattern0, pattern1, pattern2 and pattern 3
+    Where, A,B and C represent the three inputs, alpha and beta represent the two attributes.
+    """
+    # pattern0: alpha*A*B + beta*C
+    pattern0 = \
+        OpTypePattern('Add', name='add', inputs=[
+            OpTypePattern('Mul', name='mul1', inputs=[
+                OpTypePattern('Const', name='alpha'),
+                OpTypePattern('MatMul', name='matmul', inputs=[
+                    OpTypePattern('*', name='A'),
+                    OpTypePattern('*', name='B'),
+                ]),
+            ]),
+            OpTypePattern('Mul', name='mul2', inputs=[
+                OpTypePattern('Const', name='beta'),
+                OpTypePattern('*', name='C'),
+            ])
+        ])
+
+    # pattern1: alpha*A*B + C
+    pattern1 = \
+        OpTypePattern('Add', name='add', inputs=[
+            OpTypePattern('Mul', name='mul1', inputs=[
+                OpTypePattern('MatMul', name='matmul', inputs=[
+                    OpTypePattern('*', name='A'),
+                    OpTypePattern('*', name='B'),
+                ]),
+                OpTypePattern('Const', name='alpha')
+            ]),
+            OpTypePattern('*', name='C'),
+        ])
+
+    # pattern2: A*B + beta*C
+    pattern2 = \
+        OpTypePattern('Add', name='add', inputs=[
+            OpTypePattern('MatMul', name='matmul', inputs=[
+                OpTypePattern('*', name='A'),
+                OpTypePattern('*', name='B'),
+            ]),
+            OpTypePattern('Mul', name='mul2', inputs=[
+                OpTypePattern('*', name='C'),
+                OpTypePattern('Const', name='beta')
+            ]),
+        ])
+
+    # pattern3: A*B + C
+    pattern3 = \
+        OpTypePattern('Add', name='add', inputs=[
+            OpTypePattern('MatMul', name='matmul', inputs=[
+                OpTypePattern('*', name='A'),
+                OpTypePattern('*', name='B'),
+            ]),
+            OpTypePattern('*', name='C'),
+        ])
+
+    # pattern4: beta*C + alpha*A*B
+    pattern4 = \
+        OpTypePattern('Add', name='add', inputs=[
+            OpTypePattern('Mul', name='mul2', inputs=[
+                OpTypePattern('Const', name='beta'),
+                OpTypePattern('*', name='C'),
+            ]),
+            OpTypePattern('Mul', name='mul1', inputs=[
+                OpTypePattern('Const', name='alpha'),
+                OpTypePattern('MatMul', name='matmul', inputs=[
+                    OpTypePattern('*', name='A'),
+                    OpTypePattern('*', name='B'),
+                ]),
+            ])
+        ])
+
+    patternList = [pattern0, pattern1, pattern2, pattern3, pattern4]
+
+    for patternID, tempPattern in enumerate(patternList):
+        matcher = GraphMatcher(tempPattern, allow_reorder=True)
+        match_results = list(matcher.match_ops(ops))
+        if len(match_results)>0:
+            for match in match_results:
+                add_node = match.get_op('add')
+                matmul_node = match.get_op("matmul")
+                inputA_node = match.get_op("A")
+                inputB_node = match.get_op("B")
+                inputC_node = match.get_op("C")
+                a_edge_name = inputA_node.output[0]
+                b_edge_name = inputB_node.output[0]
+                c_edge_name = inputC_node.output[0]
+                attr4makenode = {}
+
+                if patternID == 0:  # pattern 0: alpha*A*B + beta*C
+                    mul2_node = match.get_op("mul2")
+                    mul1_node = match.get_op("mul1")
+                    alpha = match.get_op("alpha").get_tensor_value()
+                    beta = match.get_op("beta").get_tensor_value()
+                    attr4makenode = {"alpha": alpha, "beta": beta}
+
+                if patternID == 1:  # pattern1: alpha*A*B + C
+                    mul1_node = match.get_op("mul1")
+                    alpha = match.get_op("alpha").get_tensor_value()
+                    attr4makenode = {"alpha": alpha}
+
+                if patternID == 2:  # pattern2: A*B + beta*C
+                    mul2_node = match.get_op("mul2")
+                    beta = match.get_op("beta").get_tensor_value()
+                    attr4makenode = attr={"beta": beta}
+
+                # if the pattern is 3, do nothing
+                gemm = g.make_node("Gemm", inputs=[a_edge_name, b_edge_name, c_edge_name],
+                                   attr=attr4makenode,
+                                   shapes=[g.get_shape(add_node.output[0])],
+                                   dtypes=[g.get_dtype(add_node.output[0])])
+                if patternID == 0 or patternID == 1:
+                    ops.remove(mul1_node)
+                if patternID == 0 or patternID == 2:
+                    ops.remove(mul2_node)
+
+            ops.append(gemm)
+            g.replace_all_inputs(ops, add_node.output[0], gemm.output[0])
+    return ops
